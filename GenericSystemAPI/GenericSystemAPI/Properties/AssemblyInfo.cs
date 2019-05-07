230889766954065ec2dc87f16f8a2270c778ce07
Jordi Jolink <info@soneritics.nl>
2019-05-07 11:18:59 +0200

    Add NuGet package creation

--- GenericSystemAPI/GenericSystemAPI/Properties/AssemblyInfo.cs
+++ GenericSystemAPI/GenericSystemAPI/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("GenericSystemAPI")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("API for the Soneritics Generic System")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Soneritics")]
 [assembly: AssemblyProduct("GenericSystemAPI")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
